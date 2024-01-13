17634ea47cac268d82da61a7539b53474d88f77b
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 03:07:16 +0500

    NODEJS Assignment Q14
    14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Q14.ts
@@ -0,0 +1,6 @@
+// 13.Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guestArr: string[] = ["Rizwan Akhtar ", "Ahsen Rizwan", "Akhtar Hussain"];
+guestArr.map((item) =>
+  console.log(`Dear ${item}, You are invited to a Dinner`)
+);
